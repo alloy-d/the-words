357074a307667cdc67b66f9a9ba00b75399a49f7
Adam Lloyd <adam@alloy-d.net>
2011-01-31 17:38:40 -0500

    design.Stupidity--

---
+++ misc/dictionary.go
@@ -0,0 +1,151 @@
+package dictionary
+
+import (
+    "bufio"
+    "fmt"
+    "os"
+    "regexp"
+    "strings"
+)
+
+// character to index
+func ctoi(char byte) uint {
+    return uint(char - 'a')
+}
+// index to character
+func itoc(index uint) byte {
+    return byte(index + 'a')
+}
+
+// Assumption: the alphabet can't be larger than 64 characters.
+const AlphabetSize = 26
+type Word struct {
+    Mask uint64
+    Occurrences [AlphabetSize]uint
+    NumChars uint
+    MostUncommonChar byte
+    Text string
+}
+
+func NewWord(text string) *Word {
+    w := new(Word)
+    w.Text = text
+    w.generateMaskAndOccurrences()
+    return w
+}
+
+func (w *Word) generateMaskAndOccurrences() {
+    for i := 0; i < len(w.Text); i++ {
+        index := ctoi(w.Text[i])
+        w.Mask |= (1 << index)
+        w.Occurrences[index] += 1
+        w.NumChars += 1
+    }
+}
+
+func (w *Word) determineMostUncommonCharacter(chars *CharacterData) {
+    var freq float64 = 1.0
+    var char byte = 'a'
+
+    for i := uint(0); i < AlphabetSize; i++ {
+        if w.Mask & (1 << i) != 0 {
+            if chars.Frequencies[i] < freq {
+                freq = chars.Frequencies[i]
+                char = itoc(i)
+            }
+        }
+    }
+
+    w.MostUncommonChar = char
+}
+
+func (w Word) String() string {
+    space := ""
+    if len(w.Text) <= 4 { space += "\t" }
+    if len(w.Text) <= 12 { space += "\t" }
+    if len(w.Text) <= 20 { space += "\t" }
+    return fmt.Sprintf("\"%s\":%s%d characters\tmask %026b\toccurrences %v\tmost uncommon char %c", w.Text, space, w.NumChars, w.Mask, w.Occurrences, w.MostUncommonChar)
+}
+
+
+type CharacterData struct {
+    Occurrences [AlphabetSize]uint64
+    Frequencies [AlphabetSize]float64
+}
+
+func (c CharacterData) String() string {
+    s := "Character Data: --------------------------------------------\n"
+    for i := uint(0); i < AlphabetSize; i++ {
+        s += fmt.Sprintf("  '%c' occurred %10d times with a frequency of %0.4f\n", itoc(i), c.Occurrences[i], c.Frequencies[i])
+    }
+    s += "------------------------------------------------------------\n"
+
+    return s
+}
+
+
+type Dictionary struct {
+    Words []*Word
+    *CharacterData
+}
+
+func New(fileName string) *Dictionary {
+    d := new(Dictionary)
+    d.CharacterData = new(CharacterData)
+    d.readWords(fileName)
+    d.processAlphabetData()
+    return d
+}
+
+func (d *Dictionary) readWords(fileName string) {
+    file, err := os.Open(fileName, os.O_RDONLY, 0000)
+    defer file.Close()
+    if err != nil {
+        os.Exit(1)
+    }
+    reader := bufio.NewReader(file)
+
+    // TODO: this should be done better
+    charRegexp, err := regexp.Compile("^[a-z]+$")
+    if err != nil {
+        os.Exit(1)
+    }
+
+    for {
+        if word, err := reader.ReadString('\n'); err == nil {
+            word = strings.TrimSpace(word)
+            if !charRegexp.MatchString(word) {
+                continue
+            }
+            w := NewWord(word)
+            d.Words = append(d.Words, w)
+        } else {
+            break
+        }
+    }
+}
+
+func (d *Dictionary) processAlphabetData() {
+    // count characters
+    for _, w := range d.Words {
+        for i := 0; i < len(w.Text); i++ {
+            index := ctoi(w.Text[i])
+            d.Occurrences[index] += 1
+        }
+    }
+
+    var total uint64 = 0
+    for i := 0; i < AlphabetSize; i++ {
+        total += d.Occurrences[i]
+    }
+
+    // calculate frequencies
+    for i := 0; i < AlphabetSize; i++ {
+        d.Frequencies[i] = float64(d.Occurrences[i]) / float64(total)
+    }
+
+    for _, w := range d.Words {
+        w.determineMostUncommonCharacter(d.CharacterData)
+    }
+}
+
--- misc/toolkit.go
+++ misc/toolkit.go
@@ -1,145 +1,27 @@
 package main
 
 import (
-    "bufio"
+    "./dictionary"
     "flag"
     "fmt"
-    "os"
-    "regexp"
-    "strings"
 )
 
-// Assumption: the alphabet can't be larger than 64 characters.
-const AlphabetSize = 26
-type Word struct {
-    mask uint64
-    occurrences [AlphabetSize]uint
-    numChars uint
-    mostUncommonChar byte
-    text string
-}
-
-func (w Word) String() string {
-    space := ""
-    if len(w.text) <= 4 { space += "\t" }
-    if len(w.text) <= 12 { space += "\t" }
-    if len(w.text) <= 20 { space += "\t" }
-    return fmt.Sprintf("\"%s\":%s%d characters\tmask %026b\toccurrences %v\tmost uncommon char %c", w.text, space, w.numChars, w.mask, w.occurrences, w.mostUncommonChar)
-}
-
-type CharacterData struct {
-    occurrences [AlphabetSize]uint64
-    frequencies [AlphabetSize]float64
-}
-
-var Alphabet = new(CharacterData)
-
-var Dictionary []*Word
-
 var dictionaryFile *string = flag.String("dict", "/usr/share/dict/words", "Dictionary file to use")
-
-// character to index
-func ctoi(char byte) uint {
-    return uint(char - 'a')
-}
-// index to character
-func itoc(index uint) byte {
-    return byte(index + 'a')
-}
-
-func readWords() {
-    file, err := os.Open(*dictionaryFile, os.O_RDONLY, 0000)
-    defer file.Close()
-    if err != nil {
-        os.Exit(1)
-    }
-    reader := bufio.NewReader(file)
-
-    // TODO: this should be done better
-    charRegexp, err := regexp.Compile("^[a-z]+$")
-    if err != nil {
-        os.Exit(1)
-    }
-
-    for {
-        if word, err := reader.ReadString('\n'); err == nil {
-            word = strings.TrimSpace(word)
-            if !charRegexp.MatchString(word) {
-                continue
-            }
-            w := MakeWord(word)
-            Dictionary = append(Dictionary, w)
-        } else {
-            break
-        }
-    }
-}
-
-func processAlphabetData() {
-    // count characters
-    for _, w := range Dictionary {
-        for i := 0; i < len(w.text); i++ {
-            index := ctoi(w.text[i])
-            Alphabet.occurrences[index] += 1
-        }
-    }
-
-    var total uint64 = 0
-    for i := 0; i < AlphabetSize; i++ {
-        total += Alphabet.occurrences[i]
-    }
-
-    // calculate frequencies
-    for i := 0; i < AlphabetSize; i++ {
-        Alphabet.frequencies[i] = float64(Alphabet.occurrences[i]) / float64(total)
-    }
-}
-
-
-func MakeWord(text string) *Word {
-    w := new(Word)
-    w.text = text
-    w.generateMaskAndOccurrences()
-    return w
-}
-
-func (w *Word) generateMaskAndOccurrences() {
-    for i := 0; i < len(w.text); i++ {
-        index := ctoi(w.text[i])
-        w.mask |= (1 << index)
-        w.occurrences[index] += 1
-        w.numChars += 1
-    }
-}
-
-func (w *Word) determineMostUncommonCharacter() {
-    var freq float64 = 1.0
-    var char byte = 'a'
-
-    for i := uint(0); i < AlphabetSize; i++ {
-        if w.mask & (1 << i) != 0 {
-            if Alphabet.frequencies[i] < freq {
-                freq = Alphabet.frequencies[i]
-                char = itoc(i)
-            }
-        }
-    }
-
-    w.mostUncommonChar = char
-}
+var printWords *bool = flag.Bool("print-words", false, "Whether to print all word data")
+var printChars *bool = flag.Bool("print-chars", true, "Whether to print alphabet data")
 
 func main() {
-    readWords()
-    processAlphabetData()
+    flag.Parse()
+    d := dictionary.New(*dictionaryFile)
 
-    for _, w := range Dictionary {
-        w.determineMostUncommonCharacter()
+    if *printWords {
+        for _, w := range d.Words {
+            fmt.Printf("%+v\n", w)
+        }
     }
 
-    for _, w := range Dictionary {
-        fmt.Printf("%+v\n", w)
+    if *printChars {
+        fmt.Printf("%+v", d)
     }
-
-    fmt.Printf("%+v\n", Alphabet)
 }
 
